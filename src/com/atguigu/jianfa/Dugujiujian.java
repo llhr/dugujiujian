3a950818c592652f847aa94ee2d82e556848baa8
llhr <842035551@qq.com>
2017-08-18 01:08:32 +0800

    update

--- src/com/atguigu/jianfa/Dugujiujian.java
+++ src/com/atguigu/jianfa/Dugujiujian.java
@@ -3,7 +3,7 @@ package com.atguigu.jianfa;
 public class Dugujiujian {
 
 	public static void main(String[] args) {
-		System.out.println("dugujiujian tianxiawudi");
+		System.out.println("dugujiujian999 tianxiawudi");
 		
 
 	}
